ea6d1d118b77ca2a10c28a73c480288f33ae42d4
eneim <1776230+eneim@users.noreply.github.com>
2022-01-07 18:38:14 +0900

    Setup

---
+++ app-compose/build.gradle.kts
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2022. Nam Nguyen, nam@ene.im
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+  id("com.android.application")
+  id("org.jetbrains.kotlin.android")
+}
+
+android {
+  compileSdk = libs.versions.build.compileSdk.get().toInt()
+
+  defaultConfig {
+    applicationId = "app.wasabi"
+    minSdk = libs.versions.build.minSdk.get().toInt()
+    targetSdk = libs.versions.build.targetSdk.get().toInt()
+    versionCode = 1
+    versionName = "1.0"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+    vectorDrawables {
+      useSupportLibrary = true
+    }
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = true
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
+    }
+
+    debug {
+      applicationIdSuffix = ".dev"
+    }
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+
+  kotlinOptions {
+    jvmTarget = "11"
+    freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
+  }
+
+  buildFeatures {
+    viewBinding = true
+    compose = true
+  }
+
+  composeOptions {
+    kotlinCompilerExtensionVersion = libs.versions.jetpack.compose.compiler.get()
+  }
+
+  packagingOptions {
+    resources.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+  }
+}
+
+dependencies {
+  implementation(libs.androidx.core)
+  implementation(libs.androidx.lifecycle.runtime)
+
+  implementation(libs.compose.ui)
+  implementation(libs.compose.ui.preview)
+  implementation(libs.compose.material)
+  implementation(libs.compose.activity)
+  implementation(libs.compose.viewmodel)
+
+  testImplementation(libs.test.junit.core)
+  androidTestImplementation(libs.test.androidx.junit)
+  androidTestImplementation(libs.test.androidx.espresso.core)
+  androidTestImplementation(libs.test.compose.ui.junit)
+  debugImplementation(libs.test.compose.ui.tool)
+}
